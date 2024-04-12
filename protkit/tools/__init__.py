6af207fe6520a5fb3033b5dc22bcaf1abebf1ce5
Fred Senekal <fred@silicogenesis.com>
2024-04-12 12:41:26 +0200

    Updated CHANGELOG.MD and Documentation.

--- protkit/tools/__init__.py
+++ protkit/tools/__init__.py
@@ -3,4 +3,6 @@
 These include tasks such as docking, folding, affinity prediction, and more.
 """
 
-# from .freesasa_adaptor import FreeSASAAdaptor
+from .freesasa_adaptor import FreeSASAAdaptor
+from .pdb2pqr_adaptor import PDB2PQRAdaptor
+from .reduce_adaptor import ReduceAdaptor
